a79a775277e1f81df9c8506e0b56736cf3093382
ivan.navas <ivan.navas@socin.com.br>
2025-04-02 12:17:52 -0300

    fix: filename.

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-    print("Hello from Lambda!")
+    print("Hello from Lambda! in ci cd!")
     return {
         'statusCode': 200,
         'body': 'Hello from your Lambda Function!'
